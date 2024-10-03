9fadadfa537d33777bcc43dc01358521145895e4
Your Nameoeangg <msubhan33@gmail.com>
2024-10-04 00:03:45 +0700

    install font awesome to project

---
+++ src/app/search/page.js
@@ -0,0 +1,11 @@
+export default function SearchDestinasiPage() {
+  return (
+    <div>
+      <h1>Mau Kemana?</h1>
+      <form action="">
+        <input type="text" name="city" id="city" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
